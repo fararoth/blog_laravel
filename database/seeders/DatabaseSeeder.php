5bb95a2c1fb78e0c895a4c2459f29a2902db3493
Luis Farias <luisfariasmusicista@gmail.com>
2023-07-10 11:59:33 -0400

    Base de datos, seeders, factories, etc

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,13 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
+use App\Models\Tag;
+use App\Models\Image;
+use App\Models\Post;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,6 +18,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        Storage::makeDirectory('post');
+
+        $this->call(UserSeeder::class);
+        Category::factory(4)->create();
+        Tag::factory(8)->create();
+        $this->call(PostSeeder::class);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
